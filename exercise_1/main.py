6e8a7ade1c8f159a9f205eae0cbc16403c91d35d
adigeorg <adigeorg@iu.edu>
2025-02-04 12:03:35 -0500

    Changes made to main.py

--- exercise_1/main.py
+++ exercise_1/main.py
@@ -5,11 +5,40 @@ for key, values in hospital_data.items():
 
 print("\n")
 
-def medications(hospital_data):
-    print("Medications prescribed to each patient:")
+def medications(hospital_data, patient_name):
+    print(f"Medications prescribed to {patient_name}:")
 for key, values in hospital_data.items():
-    print(f"Medications prescribed to {values['name']}:")
-    for k, v in values['medications'].items():
-        print(f" {k}: {v['dose']} {v['frequency']}")
-    print()
+    if values["name"] == patient_name:
+        for k, v in values["medications"].items():
+            print(f" {k}: {v["dose"]} {v["frequency"]}")
+        return
+
+medications(hospital_data, "Jenny Joseph")
+
+print("\n")
+
+def unique_con(hospital_data):
+
+    unique_conditions = set()
+
+    print("All unique conditions across all patients:")
+
+    for x in hospital_data.values():
+        for y in x["medical_condition"]:
+            unique_conditions.add(y)
+
+    for y in unique_conditions:
+        print(y)
+
+unique_con(hospital_data)
+
+    print("\n")
+
+def doctor_assigned(hospital_data, patient_name):
+    for j in hospital_data.values():
+        if j["name"] == patient_name:
+            print(f'{j["doctor_name"]} is the doctor treating {j["name"]}')
+            return
+doctor_assigned(hospital_data, "Peter George")
+
 
